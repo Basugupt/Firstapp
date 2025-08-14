efc4d6b82d412f0fbe88d9e7d69b8bc92d444514
Gupta <basudeo-prasad.gupta@capgemini.com>
2025-08-14 14:34:39 +0530

    Chan5fgdfdffdfgfg6

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+EmploeeankAccount emp = new EmploeeankAccount();
+emp.Actnid = 1;
+emp.accountid = 232454545;
+emp.id = 2;

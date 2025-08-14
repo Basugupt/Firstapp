fb85b777509b88cda4b5d38aaf59dfe3ef750291
Gupta <basudeo-prasad.gupta@capgemini.com>
2025-08-14 13:50:32 +0530

    Change 1

--- obj/Debug/net9.0/Firstapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/Firstapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Firstapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d854648a553a3638f3afdc042a7c1e6fa2fb11ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Firstapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Firstapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

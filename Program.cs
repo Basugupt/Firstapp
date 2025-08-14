62724a4510c8f1736c577fd9b76ee87343c888a7
Gupta <basudeo-prasad.gupta@capgemini.com>
2025-08-14 14:33:34 +0530

    Chan5fgdfdff6

--- Program.cs
+++ Program.cs
@@ -1,14 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("this is small changes forGIthub");
-Console.WriteLine("this is  changes forGIthub in main only");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("this is small changes forGIthub");
-Console.WriteLine("*****************************************************");
-Console.WriteLine("this is small changes forGIthub");
-Console.WriteLine("this is  changes forGIthub in main only");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("this is small changes forGIthub");
-Console.WriteLine("this is  changes forGIthub in main only");
